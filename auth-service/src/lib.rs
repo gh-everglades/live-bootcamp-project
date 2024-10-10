d0d6489bd7f1e1d64ab6cdf54b3cffd177e8287c
gh <guilherme.heinen@everglades.tech>
2024-10-10 08:57:34 +0200

    Sprint 1, working on Task 4

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,39 @@
+use std::error::Error;
+
+use axum::{http::StatusCode, response::IntoResponse, routing::post, serve::Serve, Router};
+use tower_http::services::ServeDir;
+
+// This struct encapsulates our application-related logic.
+pub struct Application {
+    server: Serve<Router, Router>,
+    // address is exposed as a public field
+    // so we have access to it in tests.
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        let router = Router::new()
+            .nest_service("/", ServeDir::new("assets"))
+            // TODO: Add all other routes
+            .route("/signup", post(signup)); // Example route;
+
+        let listener = tokio::net::TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        // Create a new Application instance and return it
+        Ok(Self { server, address })
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("listening on {}", &self.address);
+        self.server.await
+    }
+}
+
+// Example route handler.
+// For now we will simply return a 200 (OK) status code.
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
